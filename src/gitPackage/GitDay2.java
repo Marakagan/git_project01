26d9957d3ae30a8fa5f55c90311e213b9a4badef
Mustafa Erdogan <erakmuh@gmail.com>
2020-03-14 21:20:14 +0100

    commit 7

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -4,7 +4,7 @@ public class GitDay2 {
 
 	
 	public static void main(String[] args) {
-		
+		int num = 10;
 		System.out.println("Hello World!");
 		System.out.println("Hello everybody!");
 		System.out.println("Check if your local is ahead of origin master");		
