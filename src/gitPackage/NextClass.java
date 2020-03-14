f8e9fb46346e28d64ee53063c684d2f3f2a07067
M. Erdogan <62161848+Marakagan@users.noreply.github.com>
2020-03-14 21:52:58 +0100

    commit nine

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -5,7 +5,7 @@ public class NextClass {
 	public static void main(String[] args) {
 
 
-  System.out.println("Reverse editing from browser");
+  System.out.println("Reverse editing from Chrome");
 
   System.out.println("You are good!! like it!!");
 
