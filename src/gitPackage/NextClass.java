ffdc2f796f9fc67d18ffd6fe32c2c6ad1bc2fd99
Mustafa Erdogan <erakmuh@gmail.com>
2020-03-14 21:55:31 +0100

    commit ten

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -5,7 +5,7 @@ public class NextClass {
 	public static void main(String[] args) {
 
 
-  System.out.println("Reverse editing from browser");
+  System.out.println("Reverse editing from Firefox");
 
   System.out.println("You are good!! like it!!");
 
