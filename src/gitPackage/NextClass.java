52663bc7defe64400b173702b9fe2eb865f20fa0
Mustafa Erdogan <erakmuh@gmail.com>
2020-03-14 21:48:16 +0100

    commit 9

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -3,11 +3,11 @@ package gitPackage;
 public class NextClass {
 
 	public static void main(String[] args) {
-		
-	  
+
+
   System.out.println("Reverse editing from browser");
-  
+
   System.out.println("You are good!! like it!!");
-		
+
 	}
 }
